2d435a7a1eddc9bb6cd0e3d4f745bcad1be8fdbf
sorahiro0830 <kattyamu0830@gmail.com>
2020-12-23 01:20:00 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import _ from 'lodash';
+import { NAME, Nijou } from './utilities';
+import * as utilities from './utilities';
+import { NAME3 as NAME_OF_SORAHIRO3 } from './utilities';
+import Lion from './utilities';
+import Lionnn from './utilities';
+
+console.log(Nijou(3));
+console.log(NAME);
+console.log(utilities.Nijou2(10));
+console.log(utilities.NAME2);
+console.log(NAME_OF_SORAHIRO3);
+console.log(Lion.say());
+console.log(Lionnn.say());
 
 function component() {
   const element = document.createElement('div');
---
+++ src/utilities.js
@@ -0,0 +1,16 @@
+export function Nijou(num) {
+  return num ** 2;
+}
+export function Nijou2(num) {
+  return num ** 2;
+}
+
+export const NAME = 'Sorahiro';
+export const NAME2 = 'Sorahiro2';
+export const NAME3 = 'Sorahiro3';
+
+export default class Lion {
+  static say() {
+    return 'Roar';
+  }
+}
