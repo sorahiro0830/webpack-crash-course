29ef7926599cc98daa787d693607b4e3a1f519bb
sorahiro0830 <kattyamu0830@gmail.com>
2020-12-29 17:22:00 +0900

    install source-map

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import './style.scss';
 
-console.log('I am printing logs.');
+// console.log('I am printing logs.');
+
+// alert('Hi, source-map!');
 
 ReactDOM.render(
   <div>Hello, React!</div>,
