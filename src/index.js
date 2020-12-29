73799d9cb20c96aefad2e95419550b6a2f868a95
sorahiro0830 <kattyamu0830@gmail.com>
2020-12-29 14:20:34 +0900

    inprement css-loader and style-loader

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import _ from 'lodash';
-import { NAME, Nijou } from './utilities';
-import * as utilities from './utilities';
-import { NAME3 as NAME_OF_SORAHIRO3 } from './utilities';
-import Lion from './utilities';
-import Lionnn from './utilities';
+import './style.css'
+// import style from './style.css'
 
-console.log(Nijou(3));
-console.log(NAME);
-console.log(utilities.Nijou2(10));
-console.log(utilities.NAME2);
-console.log(NAME_OF_SORAHIRO3);
-console.log(Lion.say());
-console.log(Lionnn.say());
+// consoleで見るとtoString()を持っていることが分かる。
+// console.log(style);
+// console.log(style.toString());
+
+// import { NAME, Nijou } from './utilities';
+// import * as utilities from './utilities';
+// import { NAME3 as NAME_OF_SORAHIRO3 } from './utilities';
+// import Lion from './utilities';
+// import Lionnn from './utilities';
+
+// console.log(Nijou(3));
+// console.log(NAME);
+// console.log(utilities.Nijou2(10));
+// console.log(utilities.NAME2);
+// console.log(NAME_OF_SORAHIRO3);
+// console.log(Lion.say());
+// console.log(Lionnn.say());
 
 function component() {
   const element = document.createElement('div');
@@ -21,3 +28,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+document.body.classList.add('haikei');
